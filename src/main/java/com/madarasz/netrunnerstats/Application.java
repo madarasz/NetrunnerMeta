96c157781682a88f23f3a3beffafecad3d480408
istvanbb <istvan@backbase.com>
2015-06-11 14:30:48 +0200

    refactoring, Operations class created

--- src/main/java/com/madarasz/netrunnerstats/Application.java
+++ src/main/java/com/madarasz/netrunnerstats/Application.java
@@ -1,9 +1,6 @@
 package com.madarasz.netrunnerstats;
 
-import com.madarasz.netrunnerstats.DOs.Card;
-import com.madarasz.netrunnerstats.DOs.CardPack;
-import com.madarasz.netrunnerstats.DOs.Deck;
-import com.madarasz.netrunnerstats.DOs.relationships.DeckHasCard;
+
 import com.madarasz.netrunnerstats.DRs.CardPackRepository;
 import com.madarasz.netrunnerstats.DRs.CardRepository;
 import com.madarasz.netrunnerstats.DRs.DeckRepository;
@@ -25,9 +22,6 @@ import org.springframework.data.neo4j.core.GraphDatabase;
 import org.springframework.data.neo4j.template.Neo4jOperations;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by madarasz on 2015-06-08.
@@ -70,6 +64,9 @@ public class Application implements CommandLineRunner {
     @Autowired
     Neo4jOperations template;
 
+    @Autowired
+    Operations operations;
+
     public void run(String... args) throws Exception {
 
         PossibleOperations op = PossibleOperations.valueOf(args[0]);
@@ -79,12 +76,12 @@ public class Application implements CommandLineRunner {
         try {
 
             switch (op) {
-                case loadnetrunnerdb: loadNetrunnerDB(false); break;
-                case updatenetrunnerdb: loadNetrunnerDB(true); break;
-                case testdb: testDb(); break;
-                case loadnetrunnerdbdeck: loadNetrunnerDbDeck(); break;
-                case loadacoodeck: loadAcooDeck(); break;
-                case cleandb: cleanDB(); break;
+                case loadnetrunnerdb: operations.loadNetrunnerDB(false); break;
+                case updatenetrunnerdb: operations.loadNetrunnerDB(true); break;
+                case testdb: operations.testDb(); break;
+                case loadnetrunnerdbdeck: operations.loadNetrunnerDbDeck(); break;
+                case loadacoodeck: operations.loadAcooDeck(); break;
+                case cleandb: operations.cleanDB(); break;
             }
 
             tx.success();
@@ -103,85 +100,5 @@ public class Application implements CommandLineRunner {
     public enum PossibleOperations {
         cleandb, deletedb, loadnetrunnerdb, updatenetrunnerdb, testdb,
         loadnetrunnerdbdeck, loadacoodeck
-    }
-
-    public void cleanDB() {
-        System.out.println("Cleaning DB.");
-        Map<String, Object> emptyparams = new HashMap<String, Object>();
-        template.query("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r", emptyparams);
-        logDBCount();
-    }
-
-    public void logDBCount() {
-        System.out.println(String.format("Cards: %d, CardPacks: %d, Decks: %d, Deck-card relations: %d",
-                template.count(Card.class), template.count(CardPack.class), template.count(Deck.class), template.count(DeckHasCard.class)));
-    }
-
-    public void loadNetrunnerDB(boolean merge) {
-        Set<CardPack> allCardPacks = netrunnerDBBroker.readSets();
-        int found = 0;
-        for (CardPack cardPack : allCardPacks) {
-            if ((!merge) || (cardPackRepository.findByCode(cardPack.getCode()) == null)) {
-                cardPackRepository.save(cardPack);
-                System.out.println("Found pack: " + cardPack.toString());
-                found++;
-            }
-        }
-        System.out.println("Found new card packs: " + found);
-
-        Set<Card> allCards = netrunnerDBBroker.readCards();
-        found = 0;
-        for (Card card : allCards) {
-            if ((!merge) || (cardRepository.findByTitle(card.getTitle()) == null)) {
-                cardRepository.save(card);
-                System.out.println("Found card: " + card.toString());
-                found++;
-            }
-        }
-        System.out.println("Found new cards: " + found);
-    }
-
-    public void testDb() {
-        logDBCount();
-
-        CardPack whatset = cardPackRepository.findByName("Core Set");
-        if (whatset != null) {
-            System.out.println(whatset.toString());
-        } else {
-            System.out.println("Card pack not found!");
-        }
-
-        Card whatcard = cardRepository.findByTitle("Account Siphon");
-        if (whatcard != null) {
-            System.out.println(whatcard.toString());
-        } else {
-            System.out.println("Card not found!");
-        }
-
-        Deck whatdeck = deckRepository.findByUrl("http://netrunnerdb.com/api//en/decklist/20162");
-        if (whatdeck != null) {
-            System.out.println(whatdeck.toString());
-        } else {
-            System.out.println("NetrunnerDB Deck not found!");
-        }
-
-        Deck whatdeck2 = deckRepository.findByUrl("http://www.acoo.net/deck/10890");
-        if (whatdeck2 != null) {
-            System.out.println(whatdeck2.toString());
-        } else {
-            System.out.println("Acoo deck not found!");
-        }
-    }
-
-    public void loadNetrunnerDbDeck() {
-        Deck deck = netrunnerDBBroker.readDeck(20162);
-        System.out.println(deck.toString());
-        deckRepository.save(deck);
-    }
-
-    public void loadAcooDeck() {
-        Deck deck = acooBroker.readDeck(10890);
-        System.out.println(deck.toString());
-        deckRepository.save(deck);
     }
 }
---
+++ src/main/java/com/madarasz/netrunnerstats/Operations.java
@@ -0,0 +1,123 @@
+package com.madarasz.netrunnerstats;
+
+import com.madarasz.netrunnerstats.DOs.Card;
+import com.madarasz.netrunnerstats.DOs.CardPack;
+import com.madarasz.netrunnerstats.DOs.Deck;
+import com.madarasz.netrunnerstats.DOs.relationships.DeckHasCard;
+import com.madarasz.netrunnerstats.DRs.CardPackRepository;
+import com.madarasz.netrunnerstats.DRs.CardRepository;
+import com.madarasz.netrunnerstats.DRs.DeckRepository;
+import com.madarasz.netrunnerstats.brokers.AcooBroker;
+import com.madarasz.netrunnerstats.brokers.NetrunnerDBBroker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.template.Neo4jOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by istvan on 2015-06-11.
+ */
+@Component
+public class Operations {
+
+    @Autowired
+    CardRepository cardRepository;
+
+    @Autowired
+    CardPackRepository cardPackRepository;
+
+    @Autowired
+    DeckRepository deckRepository;
+
+    @Autowired
+    NetrunnerDBBroker netrunnerDBBroker;
+
+    @Autowired
+    AcooBroker acooBroker;
+
+    @Autowired
+    Neo4jOperations template;
+
+    public void cleanDB() {
+        System.out.println("Cleaning DB.");
+        Map<String, Object> emptyparams = new HashMap<String, Object>();
+        template.query("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r", emptyparams);
+        logDBCount();
+    }
+
+    public void logDBCount() {
+        System.out.println(String.format("Cards: %d, CardPacks: %d, Decks: %d, Deck-card relations: %d",
+                template.count(Card.class), template.count(CardPack.class), template.count(Deck.class), template.count(DeckHasCard.class)));
+    }
+
+    public void loadNetrunnerDB(boolean merge) {
+        Set<CardPack> allCardPacks = netrunnerDBBroker.readSets();
+        int found = 0;
+        for (CardPack cardPack : allCardPacks) {
+            if ((!merge) || (cardPackRepository.findByCode(cardPack.getCode()) == null)) {
+                cardPackRepository.save(cardPack);
+                System.out.println("Found pack: " + cardPack.toString());
+                found++;
+            }
+        }
+        System.out.println("Found new card packs: " + found);
+
+        Set<Card> allCards = netrunnerDBBroker.readCards();
+        found = 0;
+        for (Card card : allCards) {
+            if ((!merge) || (cardRepository.findByTitle(card.getTitle()) == null)) {
+                cardRepository.save(card);
+                System.out.println("Found card: " + card.toString());
+                found++;
+            }
+        }
+        System.out.println("Found new cards: " + found);
+    }
+
+    public void testDb() {
+        logDBCount();
+
+        CardPack whatset = cardPackRepository.findByName("Core Set");
+        if (whatset != null) {
+            System.out.println(whatset.toString());
+        } else {
+            System.out.println("Card pack not found!");
+        }
+
+        Card whatcard = cardRepository.findByTitle("Account Siphon");
+        if (whatcard != null) {
+            System.out.println(whatcard.toString());
+        } else {
+            System.out.println("Card not found!");
+        }
+
+        Deck whatdeck = deckRepository.findByUrl("http://netrunnerdb.com/api//en/decklist/20162");
+        if (whatdeck != null) {
+            System.out.println(whatdeck.toString());
+        } else {
+            System.out.println("NetrunnerDB Deck not found!");
+        }
+
+        Deck whatdeck2 = deckRepository.findByUrl("http://www.acoo.net/deck/10890");
+        if (whatdeck2 != null) {
+            System.out.println(whatdeck2.toString());
+        } else {
+            System.out.println("Acoo deck not found!");
+        }
+    }
+
+    public void loadNetrunnerDbDeck() {
+        Deck deck = netrunnerDBBroker.readDeck(20162);
+        System.out.println(deck.toString());
+        deckRepository.save(deck);
+    }
+
+    public void loadAcooDeck() {
+        Deck deck = acooBroker.readDeck(10890);
+        System.out.println(deck.toString());
+        deckRepository.save(deck);
+    }
+}
