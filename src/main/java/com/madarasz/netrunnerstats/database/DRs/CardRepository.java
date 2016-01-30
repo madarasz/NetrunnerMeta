41401fa65db2d72419dfde3a2ac208c379da3bb2
Istvan Madarasz <mr.madarasz.istvan@gmail.com>
2016-01-30 15:59:51 +0100

    most used cards update

--- src/main/java/com/madarasz/netrunnerstats/database/DRs/CardRepository.java
+++ src/main/java/com/madarasz/netrunnerstats/database/DRs/CardRepository.java
@@ -35,6 +35,6 @@ public interface CardRepository extends GraphRepository<Card>, RelationshipOpera
     @Query("MATCH (p:CardPack {name: {0}})<-[:POOL]-(:Tournament)<-[:IN_TOURNAMENT]-(:Standing {topdeck: true})" +
             "-[:IS_DECK]->(:Deck)-[:HAS_CARD]->(c:Card {side_code: {1}})-[:IN_SET]->(cp:CardPack) " +
             "RETURN c.title AS title, COUNT(c) AS count, cp.name AS cardpack, c.faction_code AS faction " +
-            "ORDER BY count DESC LIMIT 20")
+            "ORDER BY count DESC LIMIT 30")
     List<CardCounts> findMostPopularCardsByCardPack(String cardpackname, String sidecode);
 }
