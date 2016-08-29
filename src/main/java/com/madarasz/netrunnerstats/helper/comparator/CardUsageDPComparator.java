f95045ceb816d71c1952bf551283ac7a528caca2
madarasz <istvan.madarasz@disney.com>
2016-08-29 21:09:44 +0200

    DP timeline comparison from DB

--- src/main/java/com/madarasz/netrunnerstats/helper/comparator/CardUsageDPComparator.java
+++ src/main/java/com/madarasz/netrunnerstats/helper/comparator/CardUsageDPComparator.java
@@ -8,10 +8,14 @@ import java.util.Comparator;
  * Comparator for CardUsage structure, orders by data packs.
  * Created by madarasz on 2015-06-17.
  */
-public class CardUsageDPComparator extends DPComparator implements Comparator<CardUsage>{
+public class CardUsageDPComparator implements Comparator<CardUsage>{
 
     @Override
     public int compare(CardUsage c1, CardUsage c2) {
-        return Integer.compare(getNumber(c1.getCardpacktitle()), getNumber(c2.getCardpacktitle()));
+        if (c1.getCyclenumber() == c2.getCyclenumber()) {
+            return Integer.compare(c1.getDpnumber(), c2.getDpnumber());
+        } else {
+            return Integer.compare(c1.getCyclenumber(), c2.getCyclenumber());
+        }
     }
 }
--- src/main/java/com/madarasz/netrunnerstats/helper/comparator/DPDecksComparator.java
+++ src/main/java/com/madarasz/netrunnerstats/helper/comparator/DPDecksComparator.java
@@ -8,10 +8,14 @@ import java.util.Comparator;
  * Comparator for CardUsage structure, orders by data packs.
  * Created by madarasz on 2015-06-17.
  */
-public class DPDecksComparator extends DPComparator implements Comparator<DPDecks>{
+public class DPDecksComparator implements Comparator<DPDecks>{
 
     @Override
     public int compare(DPDecks c1, DPDecks c2) {
-        return -Integer.compare(getNumber(c1.getDptitle()), getNumber(c2.getDptitle()));
+        if (c1.getCyclenumber() == c2.getCyclenumber()) {
+            return Integer.compare(c1.getDpnumber(), c2.getDpnumber());
+        } else {
+            return Integer.compare(c1.getCyclenumber(), c2.getCyclenumber());
+        }
     }
 }
