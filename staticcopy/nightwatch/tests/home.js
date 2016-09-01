08b53e312db7eea736e0d9df305fe6b8f3805cae
madarasz <istvan.madarasz@disney.com>
2016-09-01 11:52:39 +0200

    last 10 filter improvements

--- staticcopy/nightwatch/tests/home.js
+++ staticcopy/nightwatch/tests/home.js
@@ -11,10 +11,7 @@ module.exports = {
             .waitForElementVisible("//ul[@class='dropdown-menu']/li[6]", 3000) // tournament drilldown menu has at least 5 elements
             .waitForElementVisible("//div[@id='drilldowns']/a[6]", 3000) // tournament drilldown list has at least 5 elements
             .waitForElementVisible("//div[@id='blogs']/div[3]", 3000) // blogs has at least 3 elements
-            .waitForElementVisible("//iframe[contains(@title,'Facebook Social Plugin')]", 3000) // facebook
-            .waitForElementVisible("//iframe[contains(@class,'twitter-timeline')]", 3000) // facebook
-            .useCss()
-            .waitForElementVisible('iframe.iubenda-ibadge', 3000) // privacy policy
+            .waitForElementVisible("//div[contains(@class,'cc_banner-wrapper')]", 3000) // cookie warning
             .end();
     }
 };
