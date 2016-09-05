e17909598755740b4174485e7e40ec1299e7b2a5
madarasz <istvan.madarasz@disney.com>
2016-09-05 16:53:58 +0200

    adding timer to stage.sh

--- staticcopy/stage.sh
+++ staticcopy/stage.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # get staging ready
 
+start=$SECONDS
+
 ./getstatic.sh
 ./getcardstats.sh
 ./getcardimages.sh
@@ -11,9 +13,16 @@ mv content/* ../../NetrunnerMetaStatic
 cd ../../NetrunnerMetaStatic/
 http-server &
 
+test=$SECONDS
+
 # nightwatch integration tests
 cd ../NetrunnerMeta/staticcopy
 nightwatch -c nightwatch/nightwatch.json
+
+end=$SECONDS
+echo "Calculation: $(( test - start )) sec"
+echo "Tests: $(( end - test )) sec"
+echo "Total time: $(( end - start )) sec"
 
 # terminate http-server
 read -p "... Press any key to terminate http server ..." -n 1 -s
