e61369ef25e1cd073d3f1b554890901af0f40b22
madarasz <Sajt123*>
2018-01-09 15:12:32 +0100

    downloading images from cardgamedb

--- staticcopy/getcardimages.sh
+++ staticcopy/getcardimages.sh
@@ -3,6 +3,7 @@
 
 curl https://netrunnerdb.com/api/2.0/public/cards > cards.json
 cards=( $(cat cards.json | jq -r '.data[].code' ) )
+imageurls=( $(cat cards.json | jq -r '.data[].image_url' ) )
 titles=( $(cat cards.json | jq -r '.data[].title' | tr -s ' ' | tr ' ' '-' | tr '[:upper:]' '[:lower:]' | sed "s/[^a-z0-9.-]//g") )
 i=0;
 for card in "${cards[@]}"
@@ -10,8 +11,13 @@ do
     title=${titles[$i]}
     if [ ! -f "../src/main/resources/static/img/cards/netrunner-$title.png" ]; then
         image=${imagesrc[$i]}
+        imageurl=${imageurls[$i]}
         echo "Downloading card image: $title"
-        curl "https://netrunnerdb.com/card_image/$card.png" > "../src/main/resources/static/img/cards/netrunner-$title.png"
+        if [ "$imageurl" == "null" ]; then
+            curl "https://netrunnerdb.com/card_image/$card.png" > "../src/main/resources/static/img/cards/netrunner-$title.png"
+        else
+            curl $imageurl > "../src/main/resources/static/img/cards/netrunner-$title.png"
+        fi
     fi
     ((i++))
 done
