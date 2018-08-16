6df6c8c68a34643371f10ca9e078c0bf4e123064
madarasz <Sajt123*>
2018-08-16 15:45:50 +0200

    Adding Reign_and_Reverie to enums

--- src/main/java/com/madarasz/netrunnerstats/helper/Enums.java
+++ src/main/java/com/madarasz/netrunnerstats/helper/Enums.java
@@ -17,7 +17,7 @@ public class Enums {
     public enum CardCycles {
         Promos(0), Core_Set(1), Genesis(2), Creation_and_Control(3), Spin(4), Honor_and_Profit(5), Lunar(6),
         Order_and_Chaos(7), SanSan(8), Data_and_Destiny(9), Mumbad(10), Flashpoint(11), Red_Sand(12),
-        Terminal_Directive(13), Red_Sand_Part_2(14), Revised_Core_Set(20), Kitara(21);
+        Terminal_Directive(13), Red_Sand_Part_2(14), Revised_Core_Set(20), Kitara(21), Reign_and_Reverie(22);
 
         private final int cycleNumber;
 
@@ -37,5 +37,9 @@ public class Enums {
             }
             return "";
         }
+    }
+
+    public enum MwlType {
+        global_penalty, universal_faction_cost, is_restricted, deck_limit
     }
 }
